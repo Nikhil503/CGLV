055a23907879ea7fe3309335df72d1eb7e60e013
Nikhil <nikhillinga670@msitprogram.net>
2020-12-23 22:37:15 +0530

    second version of CGL

---
+++ CGL_2.1/src/TestJunit.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Nikki Geetu
+ *
+ */
+class TestJunit {
+	@Test
+	 public void testCGLOne() {
+	 Board b = new Board();
+	 System.out.println("======TEST ONE EXECUTED=======");
+	 System.out.println(b.printBoard(null));
+	     Assertions.assertEquals("Board is printed",b.printBoard(null));
+	}
+
+	 @Test
+	 public void testCGLTwo() {
+	 Board b = new Board();
+	 System.out.println("======TEST TWO EXECUTED=======");
+	     Assertions.assertSame(false,b.createboard(-5,null));
+	 
+	 }
+	@Test
+	 public void testCGLThree() {
+	 Board b = new Board();
+
+	     System.out.println("======TEST THREE EXECUTED=======");
+	     Assertions.assertSame(true,b.createboard(5,null));
+	 }
+	
+}
